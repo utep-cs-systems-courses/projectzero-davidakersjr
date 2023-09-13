9d79ec369b92a53619e3bfe0c7635f2e859bf739
davidakersjr <daakers@utep.edu>
2023-09-13 17:10:12 -0600

    Finished Lab 0

--- draw_shapes.c
+++ draw_shapes.c
@@ -25,4 +25,27 @@ void print_triangle(int leftCol, int size)
     putchar('\n');
   }
 }
-
+void print_arrow(int leftCol, int size)
+{
+  int i, j;
+  int endCol = leftCol + size;
+  for (int row = 0; row <= size; row++) {
+    int minCol = leftCol + size - row, maxCol = leftCol + size + row;
+    int col;
+    for (col = 0; col < minCol; col++) putchar(' ');
+    for (       ; col <= maxCol; col++) putchar('*');
+    putchar('\n');
+  }
+  
+  for (int row = 0; row < size; row++){
+    int col2;
+    int mid = (leftCol + size +row) /2;
+    int col3 = mid - 3;
+    
+    
+    for (col2 = 0;col2 < size; col2++) putchar(' ');
+    for (       ; col3 < mid+7; col3++) putchar('*');
+    
+    putchar('\n');
+  }
+}
--- uimain.c
+++ uimain.c
@@ -7,7 +7,7 @@ int main()
 
   while (1) { // Infinite while loop
 
-    fputs("Select which shape you want to print (Triangle = t, Square = s, Chars = c) or 'q' to quit\n> ", stdout);
+    fputs("Select which shape you want to print (Triangle = t, Square = s, Chars = c, Arrow = a, Chars 8x12 = 8) or 'q' to quit\n> ", stdout);
     fflush(stdout);		/* stdout only flushes automatically on \n */
     int c;
     while ((c = getchar()) == '\n'); /* ignore newlines */
@@ -33,6 +33,15 @@ int main()
       puts("Bye!");
       goto done; 		/* terminate */
     case '\n':
+      break;
+    case 'a':
+      puts("You selected arrow:");
+      print_arrow(5, 13);
+      break;
+    case '8':
+      puts("You selected chars 8x12:");
+      for (char c = 'A'; c < 'D'; c++)
+	print_char_8x12(c);
       break;
     default:
       printf("Unrecognized option '%c', please try again!\n", c);
